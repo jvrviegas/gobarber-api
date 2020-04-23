4905f9bd8850828d3a1dd09b7c7d8bf0fe5b214c
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-22 23:14:47 -0300

    M - App - Change cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
-import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -24,8 +24,8 @@ class App {
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
